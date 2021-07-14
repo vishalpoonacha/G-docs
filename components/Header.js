4495a1cca146fbc7ca2cfb90367eb4366f49c69d
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-07-14 23:44:46 +0530

    commit

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+function Header() {
+  return (
+    <div className="sticky top-0 z-50  flex items-center px-4 py-2 shadow-md bg-white ">
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className=" h-20 w-20 border-0"
+      >
+        <Icon name="menu" size="3xl" />
+      </Button>
+      <Icon name="description" size="5xl" color="blue" />
+      <h1 className=" ml-2 text-gray-700 text-2xl">Docs</h1>
+    </div>
+  );
+}
+
+export default Header;
