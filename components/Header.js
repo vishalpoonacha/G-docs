9370f498e27d1a5188550734338ad533115b8844
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-07-15 23:42:09 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -16,6 +16,30 @@ function Header() {
       </Button>
       <Icon name="description" size="5xl" color="blue" />
       <h1 className=" ml-2 text-gray-700 text-2xl">Docs</h1>
+      <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+        <Icon name="search" size="3xl" color="gray" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-grow px-5 text-base bg-transparent outline-none"
+        />
+      </div>
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className=" md:inline-flex ml-5 md:ml-20 h-20 w-20 border-0"
+      >
+        <Icon name="apps" size="3xl" color="gray" />
+      </Button>
+      <img
+        loading="lazy"
+        className="cursor-pointer h-12 w-12 rounded-full ml-2"
+        src="https://avatars.githubusercontent.com/u/56363146?v=4"
+        alt=""
+      />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,54 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
 import Head from "next/head";
 import Header from "../components/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Google Docs</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="data:image/x-icon;
+          base64,AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAD0hUKg9IVC
+          //SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL
+          /9IVC//SFQv/0hUL/9IVC//SFQv/0hUKg9IVC/
+          /SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SF
+          Qv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv
+          /0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC/
+          /SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//
+          SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//////////////////////////////////SFQv/0hUL/9IV
+          C//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/////////////////////////////////
+          //////////////////////SFQv/0hUL/9IVC//SFQv/0hUL/
+          9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv//////////////////////////////////////////////////////9IVC//SFQv/0hUL/9IVC//
+          SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC///////////////////////////////////////////////////////0hUL/9IVC//SFQv/0hUL/9IVC//SFQ
+          v/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC
+          //SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQ
+          v/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUKg9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUL/9IVC//SFQv/0hUKgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="
+          rel="icon"
+          type="image/x-icon"
+        />
       </Head>
       <Header />
+      <section className="bg-[#F8F9FA] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+          <div></div>
+          <Image src="https://links.papareact.com/pju" layout="fill" />
+        </div>
+      </section>
     </div>
   );
 }
